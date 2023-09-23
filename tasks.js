eb9cad18071e65efdfbd5e3e54f1789b527c0fef
Alex Usin <79891938+LxCtchr@users.noreply.github.com>
2023-09-23 19:46:10 +0600

    add js file with homework solutions

--- tasks.js
+++ tasks.js
@@ -1,36 +1,84 @@
-//-----------------------------------------------------------------------------------------------------
-const res = "B" + "a" + (1 - "hello");
-// (1 - "hello") приведет к NaN, т.к с оператором -минус- единица не будет приведена к строке,
-// соответственно, данное выражение будет расцениваться как выражение с числами,
-// а в любой ситуации, когда математическая операция не срабатывает должным образом - возвращается NaN.
+// 3) Создать объект Person несколькими способами, после создать объект Person2, чтобы в нём были доступны методы объекта Person. Добавить метод logInfo чтоб он был доступен всем объектам.
+const Person1 = {
+  showName: function () {
+    console.log('1. Name: Person')
+  }
+}
 
-// Далее идут только операторы сложения со строками,
-// данные операторы будут расцениваться как конкатенация строк,
-// при конкотенации NaN будет приведено к строке и конкатенация произойдет успешно,
-// и в константу res будет присвоена строка "BaNaN".
-console.log(res === "BaNaN") //true
-//-----------------------------------------------------------------------------------------------------
-const res2 = (true && 3) + "d";
-// (true && 3) вернет 3,
-// так как оператор && является конъюнкцией и оба операнда в выражении будут истины,
-// то в таком случае возращается последний операнд
+const Person2 = Object.create(Person1);
 
-// Далее идет оператор -плюс-,
-// который в случае со строкой "d" будет являться конкатенацией строк,
-// соответственно, число 3 будет приведено к строке,
-// и в константу res2 будет присвоена строка "3d".
-console.log(res2 === "3d") //true
-//-----------------------------------------------------------------------------------------------------
-const res3 = Boolean(true && 3) + "d";
-// По аналогии со вторым примером (true && 3) вернет 3,
-// однако, теперь данное выражение передано в функцию Boolean(),
-// которая должна привести выржения к логическому типу,
-// т.к число отличное от нуля при приведении к логическому типу вернет true,
-// то Boolean(true && 3) вернет true
+Person2.showName();
+//-------------------------------------
+const Person3 = new Object()
+Person3.showName = function () {
+  console.log('2. Name: Person')
+}
 
-// Далее по аналогии со вторым примером
-// произойдет конкатенация строк
-// true будет приведено к строке
-// и в константу res3 будет присвоена строка "trued".
-console.log(res3 === "trued") //true
-//-----------------------------------------------------------------------------------------------------
+const Person4 = {};
+Object.setPrototypeOf(Person4, Person3);
+
+Person4.showName();
+//-------------------------------------
+Object.prototype.logInfo = function () {
+  console.log('Showing some info')
+}
+Person1.logInfo();
+Person2.logInfo();
+Person3.logInfo();
+Person4.logInfo();
+
+//4) Создать класс PersonThree c get и set для поля name и конструктором, сделать класс наследник от класса Person.
+class PersonThree {
+  #name;
+
+  constructor(name) {
+    this.#name = name
+  }
+
+  get name() {
+    return this.#name
+  }
+
+  set name(value) {
+    this.#name = value; 
+  }
+}
+
+class newPerson extends PersonThree {
+  //something
+}
+
+const newObj = new newPerson("Peter");
+console.log(newObj.name)
+newObj.name = "Not Peter, now Bob";
+console.log(newObj.name)
+
+//БОНУС: 1) Написать функцию, которая вернет массив с первой парой чисел, сумма которых равна total:
+const firstSum1 = (arr, total) => {
+  const pairs = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = arr[arr.length - 1]; j > 0; j--) {
+      if (arr[i] + arr[j] === total) {
+        pairs.push(arr[i], arr[j])
+        return pairs;
+      }
+    }
+  }
+}
+
+console.log(firstSum1([1, 2, 3, 4, 5, 6, 7, 8, 9], 13)); //Выведет: (4,9) // Сложность O(n^2)
+
+const firstSum2 = (arr, total) => {
+  const map = {};
+  const pairs = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (map[arr[i]]) {
+      pairs.push(map[arr[i]], arr[i]);
+      return pairs
+    }
+    map[total - arr[i]] = arr[i];
+  }
+}
+
+console.log(firstSum2([1, 2, 3, 4, 5, 6, 7, 8, 9], 13)); //Выведет: (6,7) // Сложность O(n)
